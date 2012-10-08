97dc1e3d1c12268f8940fef04b3a1bbd4ff94bcd
johannes kutsam <johannes@kutsam.at>
2012-10-08 21:18:55 +0200

    faster

--- eurostat_r.r
+++ eurostat_r.r
@@ -7,38 +7,40 @@
 #load factors
 #save as r data object
 
-datasetname="jvs_a_nace2"
-
+datasetname="namq_gdp_c"
+datasetname="une_rt_q"
 LANGUAGE="en"
 
- 
 
 read.eurostat=function(datasetname,LANGUAGE="en",save=FALSE){
-  library(RCurl)
   library(data.table)
-  library(reshape)
-  library(stringr)
-  
+  library(reshape2)  
   baseurl="http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?sort=1&file=data%2F"
-  
   fullfilename=paste(datasetname,".tsv.gz",sep="")
   temp <- paste(tempfile(),".gz",sep="")
   download.file(paste(baseurl,fullfilename,sep=""),temp)
   dataconnection <- gzfile(temp)
-  d=read.delim(dataconnection)
-  longdata=melt(d,id=colnames(d)[1])
+  d=data.table(read.delim(dataconnection))
   
   firstname=colnames(d)[1] # remove .time and count how many headings are there 
   firstname=substr(firstname,1,nchar(firstname)-nchar(".time"))
   headings=toupper(strsplit(firstname,".",fixed=TRUE)[[1]])
   headingcount=length(headings)
-  colnames(longdata)=c("dimensions","time","value")
+  setnames(d,colnames(d)[1],c("dimensions"))
+  
+  headings_split = data.table(colsplit(d[,dimensions], pattern = "\\,",names=headings))
+  mydata=cbind(headings_split,d[,colnames(d)[-1],with=FALSE])
+  longdata=data.table(melt(mydata,id=headings))
+  setnames(longdata,"variable","time")
   
   
-  #get the data on the dimension tables
-  df=data.frame(dimensions=as.character(longdata[,"dimensions"]))
-  df = transform(df, dimensions= colsplit(dimensions, split = "\\,",names=headings))
-  dimensions=data.table(df$dimensions)
+  substrRight <- function(x, n){
+    substr(x, nchar(x)-n+1, nchar(x))
+  }
+
+  longdata$flag=as.factor(substr(longdata$value,nchar(longdata$value),nchar(longdata$value)))
+  longdata$value=as.double(substr(longdata$value,1,nchar(longdata$value)-1))
+  
   
   #download the dimension labels - save headings as better variable
   dimfile=paste("http://epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?sort=1&file=dic%2F",LANGUAGE,"%2Fdimlst.dic",sep="")
@@ -50,7 +52,6 @@ read.eurostat=function(datasetname,LANGUAGE="en",save=FALSE){
   colnames(dimdata)=c("colname","desc")
   lab=dimdata$desc
   names(lab)=dimdata$colname
-  
   #create  headings that speak for themselves for columns
   speakingheadings=as.character(lab[headings])
   
@@ -60,48 +61,31 @@ read.eurostat=function(datasetname,LANGUAGE="en",save=FALSE){
     temp <- paste(tempfile(),".gz",sep="")
     download.file(factorfile,temp)
     dataconnection <- gzfile(temp)
-    factordata=read.delim(dataconnection,header=FALSE)
-    colnames(factordata)=c(heading,paste(heading,"_desc",sep=""))
+    factordata=data.table(read.delim(dataconnection,header=FALSE))
+    setnames(factordata,colnames(factordata),c(heading,paste(heading,"_desc",sep="")))
     #join the heading to the heading dataset
-    dimensions=merge(dimensions,factordata,by=heading,all.x=TRUE)
+    longdata=merge(longdata,factordata,by=heading,all.x=TRUE)
   }
   
   
     #at the end at speaking headings
-  setnames(dimensions,colnames(dimensions)[1:length(speakingheadings)],speakingheadings)
-  
-    #add data columns by writing and reading again---FASTER ;-)
-  temp=tempfile()
-  values=data.frame(value=as.character(longdata$value),stringsAsFactors=FALSE)
-  v=try(transform(values, value= colsplit(value, split = "\\ ",names=c("value","flag"))),silent=TRUE)
-  
-  
-  if(class(v)=="try-error"){
-    print("there are no flags")
-    values=data.table(values)
-    values$value=as.character(values$value)
-    values$value=as.double(values$value)
-  }else{
-    values=v$value
-    values=data.table(values)
-    values$value=as.character(values$value)
-    values$value=as.double(values$value)
-    values$flag=as.character(values$flag)
-    values[value==flag,flag:=NA]
-  }
-  
-  #fix the time column
-  identifytimedim=as.character(longdata$time[1])
-  
-  #TODO continue here: identify Q for quarter, M for month, - if non YEAR
-  if found Q then zoo -> Q  else zoo -> M
-  as.yearqtr("200706", "%Y%m")
-  grep(,"\\")
-  
-  
-  eurostatdata=cbind(dimensions,time=longdata$time,values) 
-  if(save) save(eurostatdata,file=paste(datasetname,".RData",sep=""))
-  eurostatdata
+  setnames(longdata,headings,speakingheadings)
+     
+  longdata
+#   
+#   #fix the time column X2000 or 
+#   identifytimedim=as.character(longdata$time[1])
+#   
+#   #TODO continue here: identify Q for quarter, M for month, - if non YEAR
+#   if found Q then zoo -> Q  else zoo -> M
+#   as.yearqtr("200706", "%Y%m")
+#   grep(,"\\")
+#   
+#   
+#   eurostatdata=cbind(dimensions,time=longdata$time,values) 
+#   if(save) save(eurostatdata,file=paste(datasetname,".RData",sep=""))
+#   eurostatdata
 }
   
+ 
   
