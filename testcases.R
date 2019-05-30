c49a246d76a3696e4ccb15e39edac8aeec391a8e
Johannes Behr-Kutsam <johannes.kutsam@gmail.com>
2019-05-30 10:25:47 +0200

    Update testcases.R
    das ist ein neuer commit

--- testcases.R
+++ testcases.R
@@ -1,6 +1,9 @@
 #test cases
 
+##doesnt work as of 2019-5-30
+
 #test cases  for yearly, quarterly and monthly data
 y=read.eurostat("namq_gdp_c",cache=FALSE)
 q=read.eurostat("une_rt_q")
 m=read.eurostat("prc_hicp_midx")
+
